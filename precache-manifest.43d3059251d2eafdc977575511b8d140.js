43b82c7f2328055dd624c85f5a597d153359a9da
developervilchez <developervilchez@gmail.com>
2018-10-10 16:31:28 -0500

    Updates

---
+++ precache-manifest.43d3059251d2eafdc977575511b8d140.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "180f923038051d0b454f",
+    "url": "/calculator-react/static/js/runtime~main.180f9230.js"
+  },
+  {
+    "revision": "5b64e1d47fb101f60f67",
+    "url": "/calculator-react/static/js/main.5b64e1d4.chunk.js"
+  },
+  {
+    "revision": "f72fb35097df67971c3f",
+    "url": "/calculator-react/static/js/1.f72fb350.chunk.js"
+  },
+  {
+    "revision": "5b64e1d47fb101f60f67",
+    "url": "/calculator-react/static/css/main.8076f28e.chunk.css"
+  },
+  {
+    "revision": "b3918c1398c41c465000e9dd4be7a777",
+    "url": "/calculator-react/index.html"
+  }
+];
